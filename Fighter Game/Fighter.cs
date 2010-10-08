4a9dc7becb0e30bdfcf1097967badb6129fbede2
James & Eric Kinyon <erickinyon@live.com>
2010-10-08 16:26:33 -0400

    Example commit. Not important. Just a test.

---
+++ Fighter Game/Fighter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fighter_Game
+{
+    class Fighter
+    {
+        // This is an example. Just to show git is working fine.
+    }
+}
